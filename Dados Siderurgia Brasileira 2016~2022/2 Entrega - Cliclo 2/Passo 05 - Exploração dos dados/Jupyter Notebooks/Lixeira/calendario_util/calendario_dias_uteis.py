8527059ac6a8549526bbe9d3974031d3132f8af5
Kaue Mandarino <kauemandarino@gmail.com>
2022-09-25 15:09:03 -0300

    updt

---
+++ Dados Siderurgia Brasileira 2016~2022/2 Entrega - Cliclo 2/Passo 05 - Exploração dos dados/Jupyter Notebooks/Lixeira/calendario_util/calendario_dias_uteis.py
@@ -0,0 +1,21 @@
+def ultimo_util(ano_inicial, ano_final):
+    from datetime import date
+    from calendar import monthrange
+    from workalendar.america import Brazil
+    meses = {1:'Jan', 2:'Fev', 3:'Mar', 4:'Abr', 5:'Mai', 6: 'Jun', 7: 'Jul', 8: 'Ago', 9: 'Set', 10: 'Out', 11: 'Nov', 12: 'Dez'}
+    datas = []
+    primeiro_dia_mes = 1
+    cal = Brazil()
+    for ano in range(ano_inicial, ano_final+1):
+        for mes in meses.keys():
+            ultimo_dia_mes = monthrange(ano,mes)
+            ultimo_dia_mes = ultimo_dia_mes[1]
+            total_dias_uteis_mes = cal.get_working_days_delta(date(ano, mes, primeiro_dia_mes), date(ano, mes, ultimo_dia_mes))
+            for dia in range(ultimo_dia_mes, 24, -1):
+                if cal.is_working_day(date(ano, mes, dia)) == True:
+                    ultimo_dia_util = dia
+                    x = date(ano, mes, dia)
+                    datas.append(str(x))
+                    break
+    return datas
+
